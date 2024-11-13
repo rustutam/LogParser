6659d3ee2240eb5bd4dc224ad7872218f43e1826
rustutam <Rustam.Asbapov@urfu.me>
2024-11-14 01:19:58 +0500

    feat: add in LogFileReader process of several files

--- src/main/java/backend/academy/readers/LogFileReader.java
+++ src/main/java/backend/academy/readers/LogFileReader.java
@@ -1,28 +1,34 @@
 package backend.academy.readers;
 
-import lombok.extern.log4j.Log4j2;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Stream;
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class LogFileReader implements Reader {
-    private final String filePath;
+    private final List<String> filePaths;
 
-    public LogFileReader(String filePath) {
-        this.filePath = filePath;
+    public LogFileReader(List<String> filePaths) {
+        this.filePaths = filePaths;
     }
 
     @Override
     public Stream<String> readLogs() {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filePath));
-            return br.lines();
-        } catch (IOException e) {
-            log.error("Ошибка в {}", e.getClass());
-        }
-        return Stream.empty();
+        return filePaths.stream()
+            .flatMap(filePath -> {
+                try {
+
+                    BufferedReader br = new BufferedReader(new FileReader(filePath));
+                    return br.lines();
+                } catch (IOException e) {
+                    log.error("Ошибка в {}", filePath, e);
+                    return Stream.empty();
+                }
+
+            });
 
     }
 
