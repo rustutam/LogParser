e76544967a6c29e45949e287ca43f85de1ffb7ee
rustutam <Rustam.Asbapov@urfu.me>
2024-11-14 14:37:42 +0500

    feat: add url checker

---
+++ src/main/java/backend/academy/URLChecker.java
@@ -0,0 +1,15 @@
+package backend.academy;
+
+import java.net.URI;
+
+public class URLChecker {
+    public static boolean isValidURL(String urlString) {
+        try {
+            URI uri = new URI(urlString);
+            uri.toURL();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
