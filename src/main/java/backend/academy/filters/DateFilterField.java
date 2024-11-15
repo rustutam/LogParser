40a5dd8c0f286e2b2c890598b44fc3790b49cb82
rustutam <Rustam.Asbapov@urfu.me>
2024-11-15 13:37:01 +0500

    fix: fix bug with convert to LocalDateTime in DateFilterField class

--- src/main/java/backend/academy/filters/DateFilterField.java
+++ src/main/java/backend/academy/filters/DateFilterField.java
@@ -1,17 +1,19 @@
 package backend.academy.filters;
 
 import backend.academy.exceptions.InvalidFilterValueException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeParseException;
 import lombok.extern.log4j.Log4j2;
 import static backend.academy.config.Settings.INPUT_DATA_FORMATTER;
 
 @Log4j2
-public final class DataFilterField extends FilterField<LocalDateTime> {
+public final class DateFilterField extends FilterField<LocalDateTime> {
     @Override
     public void parseValue(String value) throws InvalidFilterValueException {
         try {
-            super.value = LocalDateTime.parse(value, INPUT_DATA_FORMATTER);
+            LocalDate date = LocalDate.parse(value, INPUT_DATA_FORMATTER);
+            super.value = date.atStartOfDay();
         } catch (DateTimeParseException e) {
             log.error("Ошибка преобразования строки {} к дате", value);
             throw new InvalidFilterValueException(
